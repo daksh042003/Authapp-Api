4d499d11444468d3992bda0972eac7de1ce71fda
Daksh Pandey <dakshpandey127@gmail.com>
2023-08-31 23:02:28 +0530

    changes

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,5 +1,9 @@
   const User=require("../models/User");
   const bcrypt = require("bcrypt");
+  const jwt = require("jsonwebtoken");
+  require("dotenv").config();
+
+
 
 
   exports.signup=async(req,res)=>{
@@ -49,5 +53,73 @@
      }
   
 
+      
 
+      exports.login = async(req,res) =>{
+        try{
+            
+            const {email,password} = req.body;
+
+            if(!email || !password){
+                return res.status(400).json({
+                    success:false,
+                    message:'please fill all the details carefully',
+                });
+            }
+
+            const user =await User.findOne({email});
+            if(!user){
+                return res.status(401).json({
+                    success:false,
+                    message:'User is not registered',
+                });
+            }
+
+
+            const payload = {
+                email:user.email,
+                id:user._id,
+                role:user.role,
+
+            }
+
+
+            if(await bcrypt.compare(password,user.password)){
+               let token =jwt.sign(payload,
+                                   process.env.JWT_SECRET,
+                                  { 
+                                    expiresIn:"2h",
+                                     
+                                    });
+
+                // user = user.toObject();
+                user.token = token;
+                user.password = undefined;
+               const options = {
+                 httpOnly:true,
+               }
+                 
+               res.cookie("token",token,options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:'user Logged In successfully',
+               });
+                
+            }
+         else{
+                return res.status(403).json({
+                    success:false,
+                    message:"Password Incorrect",
+                })
+          }
+        }
+        catch(error){
+            console.log(error);
+             return res.status(500).json({
+                success: false,
+                message:'Login Failure',
+             });
+        }
+      }
    
