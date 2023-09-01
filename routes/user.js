9fc4c7ca5a32e0cd92231d5d65ec2bc4fcd6edbe
Daksh Pandey <dakshpandey127@gmail.com>
2023-09-01 21:37:20 +0530

    changes

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,25 @@
  const router=express.Router();
 
  const {signup,login} = require("../controllers/Auth");
+ const{auth,isStudent,isAdmin}  =  require("../middlewares/auth");
+
 
  router.post("/login",login);
  router.post("/signup",signup);
+
+ router.get("/student", auth,isStudent ,(req,res)=>{
+     res.json({
+         success:true,
+         message:'welcome to the protected route for students',
+     });
+ });
+
+
+ router.get("/admin",auth,isAdmin ,(req,res)=>{
+    res.json({
+        success:true,
+        message:'welcome to the protected route for Admin ',
+    });
+});
 
   module.exports=router;
